068df648c843afaf8ffcec7ea0c2a43c4820280b
Alejandro Seguí <alesegdia@gmail.com>
2015-09-04 17:22:47 +0200

    refactoring folder

---
+++ src/test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "artecshpp/core/component.h"
+#include "artecshpp/core/componentmanager.h"
+#include "artecshpp/core/engine.h"
+
+
+int main( int argc, char** argv )
+{
+	std::cout << "ARTECSHPP" << std::endl;
+	return 0;
+}
